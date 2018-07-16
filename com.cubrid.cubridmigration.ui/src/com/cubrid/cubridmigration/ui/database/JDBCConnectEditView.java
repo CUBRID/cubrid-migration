8760201bae25138ed3548fffd0ab12f89b394c29
Chris Han <chris.han@einsware.com>
2018-07-16 18:49:31 +0900

    [TOOLS-4301] delete unnecessary comments and change curly brackets to follow Oracle's java coding convention

--- com.cubrid.cubridmigration.ui/src/com/cubrid/cubridmigration/ui/database/JDBCConnectEditView.java
+++ com.cubrid.cubridmigration.ui/src/com/cubrid/cubridmigration/ui/database/JDBCConnectEditView.java
@@ -116,15 +116,11 @@ public class JDBCConnectEditView {
 			return;
 		}		
 		if (!JDBCDriverManager.getInstance().addDriver(firstFile, false)) {
-			// --> added by chris
 			List<JDBCData> driverList = dt.getJDBCDatas();
-			JDBCData jd3 = dt.getJDBCData(firstFile) ;
-			if (jd3 != null)
-			{
-				for (JDBCData jd2 : driverList)
-				{
-					if (jd3.getDesc().equals(jd2.getDesc()))
-					{
+			JDBCData jd2 = dt.getJDBCData(firstFile) ;
+			if (jd2 != null) {
+				for (JDBCData jd3 : driverList) {
+					if (jd3.getDesc().equals(jd2.getDesc())) {
 						UICommonTool.openInformationBox(Display.getDefault().getActiveShell(),
 								Messages.msgWarning,
 								Messages.msgDuplicatedJdbcDriverFile);
@@ -132,7 +128,6 @@ public class JDBCConnectEditView {
 					}
 				}
 			}
-			// <--
 			UICommonTool.openErrorBox(Display.getDefault().getActiveShell(),
 					Messages.errInvalidJdbcJar);
 			return;
