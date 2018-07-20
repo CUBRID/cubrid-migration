dc9b8e4a5312e6a30d98a1abc94ef25ddd2772dd
Chris Han <chris.han@einsware.com>
2018-07-20 17:23:18 +0900

    [TOOLS-4301] Modified to use JDBCDriverManager Class. (Method name is 'isDriverDuplicated()')

--- com.cubrid.cubridmigration.ui/src/com/cubrid/cubridmigration/ui/database/JDBCConnectEditView.java
+++ com.cubrid.cubridmigration.ui/src/com/cubrid/cubridmigration/ui/database/JDBCConnectEditView.java
@@ -115,20 +115,12 @@ public class JDBCConnectEditView {
 		if (StringUtils.isBlank(firstFile)) {
 			return;
 		}
+		if (JDBCDriverManager.getInstance().isDriverDuplicated(firstFile)) {
+			UICommonTool.openErrorBox(Display.getDefault().getActiveShell(),
+					Messages.msgDuplicatedJdbcDriverFile);
+			return;
+		}
 		if (!JDBCDriverManager.getInstance().addDriver(firstFile, false)) {
-			List<JDBCData> driverList = dt.getJDBCDatas();
-			JDBCData jd2 = dt.getJDBCData(firstFile) ;
-			if (jd2 != null) {
-				for (JDBCData jd3 : driverList) {
-					if (jd3.getDesc().equals(jd2.getDesc())) {
-						UICommonTool.openInformationBox(Display.getDefault().getActiveShell(),
-								Messages.msgWarning,
-								Messages.msgDuplicatedJdbcDriverFile);
-
-						return;
-					}
-				}
-			}
 			UICommonTool.openErrorBox(Display.getDefault().getActiveShell(),
 					Messages.errInvalidJdbcJar);
 			return;
