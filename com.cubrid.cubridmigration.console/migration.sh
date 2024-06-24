698ffc3c675f9d2ccb11935fda490ef9ce734f3d
CUBRID <kdm97912@gmail.com>
2024-06-24 10:17:42 +0900

    fix java memory default setting

--- com.cubrid.cubridmigration.console/migration.sh
+++ com.cubrid.cubridmigration.console/migration.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 DIR=$PWD
-java -jar $DIR/com.cubrid.cubridmigration.command-1.0.0-SNAPSHOT.jar "$@"
+java -jar -Xms 1024M -Xmx 4096M $DIR/com.cubrid.cubridmigration.command-1.0.0-SNAPSHOT.jar "$@"
