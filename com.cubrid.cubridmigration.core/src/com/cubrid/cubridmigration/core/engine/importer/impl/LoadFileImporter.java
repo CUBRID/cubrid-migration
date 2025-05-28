11821dab96c1df34ad072a11702a2b580831c87e
Kang-dot <jgbu97@gmail.com>
2025-05-28 16:47:19 +0900

    [TOOLS-4704] format files
    - format edited files

--- com.cubrid.cubridmigration.core/src/com/cubrid/cubridmigration/core/engine/importer/impl/LoadFileImporter.java
+++ com.cubrid.cubridmigration.core/src/com/cubrid/cubridmigration/core/engine/importer/impl/LoadFileImporter.java
@@ -175,7 +175,8 @@ public class LoadFileImporter extends OfflineImporter {
         synchronized (lockObj) {
             MigrationDirAndFilesManager mdfm = mrManager.getDirAndFilesMgr();
             String schemaName =
-                    config.getSrcCatalog().getDatabaseType().isSupportMultiSchema() && !(stc.getOwner() == null)
+                    config.getSrcCatalog().getDatabaseType().isSupportMultiSchema()
+                                    && !(stc.getOwner() == null)
                             ? stc.getOwner()
                             : config.getSrcConnOwner();
 
@@ -183,7 +184,6 @@ public class LoadFileImporter extends OfflineImporter {
                 tableFiles.put(
                         schemaName + stc.getName(),
                         new CurrentDataFileInfo(
-                        		// TODO: toUpperCase()
                                 config.getTargetDataFileName(schemaName.toUpperCase()),
                                 mdfm.getMergeFilesDir(),
                                 config.getTargetFilePrefix(),
