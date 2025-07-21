a15b62e86ee36ca4db34fa0141b49f3b860ce1ca
Srltas <kdm97912@gmail.com>
2025-07-21 10:28:46 +0900

    fix: add Oracle datetime function

--- plugins/com.cubrid.cubridmigration.core/src/com/cubrid/cubridmigration/oracle/trans/Oracle2CUBRIDTranformHelper.java
+++ plugins/com.cubrid.cubridmigration.core/src/com/cubrid/cubridmigration/oracle/trans/Oracle2CUBRIDTranformHelper.java
@@ -386,6 +386,8 @@ public class Oracle2CUBRIDTranformHelper extends DBTransformHelper {
                 return "sys_datetime";
             case "current_date":
             case "localtimestamp":
+            case "systimestamp":
+            case "current_timestamp":
                 return "current_datetime";
         }
 
