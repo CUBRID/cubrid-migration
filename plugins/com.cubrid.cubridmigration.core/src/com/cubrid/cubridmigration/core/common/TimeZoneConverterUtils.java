090d8a320809210cef6bf3680182ca414d4f2ed7
sys0ut <j99717@naver.com>
2025-11-17 14:16:27 +0900

    Remove old copyright header (* Copyright (C) 2008 Search Solution Corporation.) from new files

--- plugins/com.cubrid.cubridmigration.core/src/com/cubrid/cubridmigration/core/common/TimeZoneConverterUtils.java
+++ plugins/com.cubrid.cubridmigration.core/src/com/cubrid/cubridmigration/core/common/TimeZoneConverterUtils.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2008 Search Solution Corporation.
+ * Copyright (C) 2016 CUBRID Corporation.
  *
  * Redistribution and use in source and binary forms, with or without modification,
  * are permitted provided that the following conditions are met:
@@ -25,6 +25,7 @@
  * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY
  * OF SUCH DAMAGE.
+ *
  */
 package com.cubrid.cubridmigration.core.common;
 
