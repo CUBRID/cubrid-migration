caecff0802dc7fcb2a4b4584fd087a5701103572
Srltas <kdm97912@gmail.com>
2026-02-24 20:27:25 +0900

    feat: add Tibero data export and migration engine integration

--- plugins/com.cubrid.cubridmigration.core/src/com/cubrid/cubridmigration/core/engine/exporter/impl/JDBCExporter.java
+++ plugins/com.cubrid.cubridmigration.core/src/com/cubrid/cubridmigration/core/engine/exporter/impl/JDBCExporter.java
@@ -479,6 +479,7 @@ public class JDBCExporter extends MigrationExporter {
         int sourceDBTypeID = config.getSourceDBType().getID();
         if (config.isImplicitEstimate()
                 && (sourceDBTypeID == DatabaseType.ORACLE.getID()
+                        || sourceDBTypeID == DatabaseType.TIBERO.getID()
                         || sourceDBTypeID == DatabaseType.MYSQL.getID())) {
             return true;
         }
