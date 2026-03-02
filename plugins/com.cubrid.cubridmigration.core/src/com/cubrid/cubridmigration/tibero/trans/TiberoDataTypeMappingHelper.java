ef68059d01df27da60647e1075b74a1cd2137f3e
Srltas <kdm97912@gmail.com>
2026-03-02 23:03:08 +0900

    refactor: encapsulate mapping key generation into a dedicated inner class

--- plugins/com.cubrid.cubridmigration.core/src/com/cubrid/cubridmigration/tibero/trans/TiberoDataTypeMappingHelper.java
+++ plugins/com.cubrid.cubridmigration.core/src/com/cubrid/cubridmigration/tibero/trans/TiberoDataTypeMappingHelper.java
@@ -29,20 +29,13 @@
  */
 package com.cubrid.cubridmigration.tibero.trans;
 
-import com.cubrid.common.log.LogUtil;
 import com.cubrid.cubridmigration.core.mapping.AbstractDataTypeMappingHelper;
 import com.cubrid.cubridmigration.tibero.TiberoDataTypeHelper;
 
-import org.apache.commons.lang3.StringUtils;
-import org.slf4j.Logger;
-
 public class TiberoDataTypeMappingHelper extends AbstractDataTypeMappingHelper {
-    private static final Logger LOG = LogUtil.getLogger(AbstractDataTypeMappingHelper.class);
 
     /**
      * @param databaseTypeID
-     * @param key
-     * @param dataTypeMappingFileName
      */
     public TiberoDataTypeMappingHelper() {
         super("TIBERO2CUBRID", "/com/cubrid/cubridmigration/tibero/trans/Tibero2CUBRID.xml");
@@ -56,76 +49,46 @@ public class TiberoDataTypeMappingHelper extends AbstractDataTypeMappingHelper {
      * @param scale String
      * @return key String
      */
+    @Override
     public String getMapKey(String datatype, String precision, String scale) {
-
-        String dataTypeUpper = datatype;
-        if ("NUMBER".equals(dataTypeUpper)) {
-            return getNumberMapKey(dataTypeUpper, precision);
-        } else if (dataTypeUpper.matches("INTERVAL DAY\\(\\d*\\) TO SECOND\\(\\d*\\)")) {
-            return "INTERVAL DAY TO SECOND";
-        } else if (dataTypeUpper.matches("INTERVAL YEAR\\(\\d*\\) TO MONTH")) {
-            return "INTERVAL YEAR TO MONTH";
-        } else {
-            return TiberoDataTypeHelper.getTiberoDataTypeKey(dataTypeUpper);
-        }
+        return new MappingKey(datatype, precision).generate();
     }
 
-    /**
-     * get the number type map key
-     *
-     * @param dataTypeUpper String
-     * @param precision String
-     * @return key String
-     */
-    private String getNumberMapKey(String dataTypeUpper, String precision) {
-        String tempPre = null;
-        String tempScale = null;
+    /** MappingKey encapsulates the logic for generating a unique mapping key from Tibero types. */
+    private final class MappingKey {
+        private final String dataType;
+        private final String precision;
 
-        if ("p".equalsIgnoreCase(precision)) {
-            tempPre = "p";
-            tempScale = "s";
-        } else {
-            Integer intPrecision = str2Integer(precision);
-            if (intPrecision != null && intPrecision > 0) {
-                tempPre = "p";
-                tempScale = "s";
+        private MappingKey(String dataType, String precision) {
+            this.dataType = dataType == null ? "" : dataType.toUpperCase();
+            this.precision = precision;
+        }
+
+        private String generate() {
+            if ("NUMBER".equals(dataType)) {
+                return generateNumberKey();
             }
+            if (dataType.matches("INTERVAL DAY\\(\\d*\\) TO SECOND\\(\\d*\\)")) {
+                return "INTERVAL DAY TO SECOND";
+            }
+            if (dataType.matches("INTERVAL YEAR\\(\\d*\\) TO MONTH")) {
+                return "INTERVAL YEAR TO MONTH";
+            }
+            return TiberoDataTypeHelper.getTiberoDataTypeKey(dataType);
         }
 
-        StringBuilder sb = new StringBuilder();
-        sb.append(dataTypeUpper);
-        sb.append(MAP_KEY_SEPARATOR);
-
-        if (tempPre != null) {
-            sb.append(tempPre);
-        }
-        sb.append(MAP_KEY_SEPARATOR);
-
-        if (tempScale != null) {
-            sb.append(tempScale);
+        private String generateNumberKey() {
+            if (isPOrNumeric(precision)) {
+                return "NUMBER" + MAP_KEY_SEPARATOR + "p" + MAP_KEY_SEPARATOR + "s";
+            }
+            return "NUMBER";
         }
 
-        return sb.toString();
-    }
-
-    /**
-     * convert the string to integer
-     *
-     * @param str String
-     * @return value Integer
-     */
-    private Integer str2Integer(String str) {
-        if (StringUtils.isBlank(str)) {
-            return null;
+        private boolean isPOrNumeric(String str) {
+            if (str == null) {
+                return false;
+            }
+            return "p".equalsIgnoreCase(str) || str.matches("^-?\\d+$");
         }
-        if (!str.matches("^-?\\d+$")) {
-            return null;
-        }
-        try {
-            return Integer.parseInt(str);
-        } catch (NumberFormatException ex) {
-            LOG.info("Can't convert String to Integer: {}", str);
-        }
-        return null;
     }
 }
