a6497bd5f4b5083fcaa51098a7b7d75c4104cdf8
Longha Seng <senglongha.tech@gmail.com>
2026-06-22 16:36:49 +0700

    Fix code style with GoogleJavaFormat

--- plugins/com.cubrid.cubridmigration.core/src/com/cubrid/cubridmigration/core/connection/ConnParameters.java
+++ plugins/com.cubrid.cubridmigration.core/src/com/cubrid/cubridmigration/core/connection/ConnParameters.java
@@ -167,7 +167,7 @@ public final class ConnParameters implements Serializable, IDBSource, IJDBCConne
     }
 
     public String getConServer() {
-    	return StringUtils.defaultIfBlank(conServer, "informix");
+        return StringUtils.defaultIfBlank(conServer, "informix");
     }
 
     public void setConServer(String conServer) {
